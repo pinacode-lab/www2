8ff53c936097ca5c235af03ce82470f19c1009aa
isaac israel <email.iisrael@gmail.com>
2019-03-24 00:19:50 -0300

    Remove the sweetalert

--- _javascript/packs/application.js
+++ _javascript/packs/application.js
@@ -1,8 +1,1 @@
 import 'bootstrap';
-import { initSweetalert } from '../components/init_sweetalert';
-
-initSweetalert('#sweet-alert-demo', {
-  title: "A nice alert",
-  text: "This is a great alert, isn't it?",
-  icon: "success"
-});
